d73198d4beb221eb1419e37528c1bda8780e6568
glvchv <60256223+glvchv@users.noreply.github.com>
2020-03-09 08:03:29 +0200

    Update user.service.ts

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -19,7 +19,7 @@ export class UserService {
   isAuthChanged = new Subject<boolean>();
 
   constructor(private afAuth: AngularFireAuth, private router: Router, private afs: AngularFirestore, private toastr: ToastrService) {
-    // ********* FIREBASE DOCUMENT WITH USER ID AND success ********
+    // ********* FIREBASE DOCUMENT WITH USER ID AND  USER DATA ********
     // this.user$ = this.afAuth.authState.pipe(switchMap(user => {
     //   if (user) {
     //     return this.afs.doc<IUser>(`users/${user.uid}`).valueChanges();
